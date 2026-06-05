ee2dea0fbb9c6352de0711f6a945b6d955028708
Notoriouszx <foufouzou2012@gmail.com>
2026-06-05 05:01:27 +0100

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,6 +3,8 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter/foundation.dart'; // ✅ ADDED for debugPrint
+
 import '../utils/constants.dart';
 
 class ApiService {
